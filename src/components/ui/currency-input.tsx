a1930237edd0e1d866c0930b9e02870e7020f93d
Ramon Moran <CRamonMB@gmail.com>
2025-07-29 05:42:16 +0000

    oye no manches, algo pasa mal. como que pongo 5000 de fondo y en total c

--- src/components/ui/currency-input.tsx
+++ src/components/ui/currency-input.tsx
@@ -14,7 +14,7 @@ interface CurrencyInputProps extends Omit<React.InputHTMLAttributes<HTMLInputEle
 const format = (value: number | undefined): string => {
   if (value === undefined || value === null) return "";
   return new Intl.NumberFormat("en-US", {
-    minimumFractionDigits: 0,
+    minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(value);
 };
@@ -33,26 +33,20 @@ const CurrencyInput = React.forwardRef<HTMLInputElement, CurrencyInputProps>(
     const [displayValue, setDisplayValue] = React.useState<string>("");
 
     React.useEffect(() => {
-        // When the external value changes, update the display value
         const numericValue = typeof value === 'string' ? parse(value) : value;
-        setDisplayValue(format(numericValue));
+        setDisplayValue(numericValue !== undefined ? format(numericValue) : "");
     }, [value]);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const inputVal = event.target.value;
         const numericValue = parse(inputVal);
-
-        // Allow user to type, even if it's not a valid number temporarily
         setDisplayValue(inputVal);
-
-        // Propagate the numeric value up
         onValueChange(numericValue);
     };
 
     const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-        // On blur, format the displayed value correctly
         const numericValue = parse(event.target.value);
-        setDisplayValue(format(numericValue));
+        setDisplayValue(numericValue !== undefined ? format(numericValue) : "");
         if (props.onBlur) {
             props.onBlur(event);
         }
